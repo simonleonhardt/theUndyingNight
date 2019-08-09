89fae1a9a102bd15c33ae8a588afee59adbe395d
Simon <simonjleonhardt@gmail.com>
2019-08-09 14:24:06 -0700

    Added ability to chop enemies, waves respawn

--- enemy.js
+++ enemy.js
@@ -37,18 +37,23 @@ class Enemy {
       let vectorX = this.x + this.width / 2 - (entity.x + entity.width / 2);
       let vectorY = this.y + this.height / 2 - (entity.y + entity.height / 2);
 
-      if (vectorX * vectorX > vectorY * vectorY) {
-        if (vectorX > 0) {
-          this.x = entity.x + entity.width;
+      if (entity.physical) {
+        if (vectorX * vectorX > vectorY * vectorY) {
+          if (vectorX > 0) {
+            this.x = entity.x + entity.width;
+          } else {
+            this.x = entity.x - this.width;
+          }
         } else {
-          this.x = entity.x - this.width;
+          if (vectorY > 0) {
+            this.y = entity.y + entity.height;
+          } else {
+            this.y = entity.y - this.height;
+          }
         }
       } else {
-        if (vectorY > 0) {
-          this.y = entity.y + entity.height;
-        } else {
-          this.y = entity.y - this.height;
-        }
+        entityArr.splice(entityArr.indexOf(this), 1);
+        enemyArr.splice(enemyArr.indexOf(this), 1);
       }
     }
   };
